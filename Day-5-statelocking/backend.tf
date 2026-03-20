a46c12dbe5fbb47180e3e0c0608b0f969b27539d
RavinaRathod <ravinarathod009@gmail.com>
2026-03-20 16:29:21 +0530

    all

--- Day-5-statelocking/backend.tf
+++ Day-5-statelocking/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "ravinarathod-383946777397-ap-south-1-an"
     key = "Day-5/terraform.tfstate"
     region = "ap-south-1"
-    use_lockfile = true
+    #use_lockfile = true
+    dynamodb_table = "test"
+    encrypt = true
   }
 }
