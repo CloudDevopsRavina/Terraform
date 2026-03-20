6309476c1b1355416c07a6e161818c4ed8f85647
RavinaRathod <ravinarathod009@gmail.com>
2026-03-20 16:54:29 +0530

    all

--- Day-5-statelocking/main.tf
+++ Day-5-statelocking/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2-server" {
     ami = "ami-0a14f53a6fe4dfcd1"
-    instance_type = "t3.micro"
+    instance_type = "t3.small"
     tags = {
-      Name = "ec2-server"
+      Name = "dev"
     }
   
 }
